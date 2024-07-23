6a89fdc76876b472f01dbc2e4830f3dbbd2c932d
John_4LenON <vasilevykhd@icloud.com>
2024-07-23 17:01:00 +0400

    Задание 3 Выполнено

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+            string name;
+            name = Console.ReadLine();
+            if(name.Any())
+            {
+                Console.WriteLine($"Приветик {name}");
+            }
+            else
+            {
+                Console.WriteLine("Приветик никто ;-)");
+            }
+        }
+    }
+}
