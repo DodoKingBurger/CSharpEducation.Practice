4abb61129260d7662676d38a8ca61d014efdd86f
John_4LenON <vasilevykhd@icloud.com>
2024-07-23 16:46:52 +0400

    Задание 1 Выполнено

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            string company;
+            bool Flag;
+            float body_weight;
+        }
+    }
+}
