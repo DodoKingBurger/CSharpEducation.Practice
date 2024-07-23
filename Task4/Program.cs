ace544a6ad0febc46454ac5b71380e51c79edef5
John_4LenON <vasilevykhd@icloud.com>
2024-07-23 17:05:31 +0400

    Задание 4 Выполнено

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+            string phrase_1 = Console.ReadLine();
+
+            string phrase_2 = Console.ReadLine();
+
+            Console.WriteLine(phrase_1 + " " + phrase_2);
+        }
+    }
+}
