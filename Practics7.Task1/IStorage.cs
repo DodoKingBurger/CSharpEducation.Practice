600c2d49cef7acfb1d6394d73f568fa187d386b7
John_4LenON <vasilevykhd@icloud.com>
2024-09-22 15:31:21 +0400

    Начал интерфейсы

---
+++ Practics7.Task1/IStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practics7.Task1
+{
+  internal interface IStorage<T>
+  {
+    /// <summary>
+    /// свойство, возвращающее общее количество элементов в хранилище.
+    /// </summary>
+    int Count { get; }
+
+    /// <summary>
+    /// Добавляет элемент `item` в хранилище.
+    /// </summary>
+    /// <param name="item"></param>
+    void Add(T item);
+    /// <summary>
+    /// возвращает элемент из хранилища по указанному индексу.
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    T Get(int index);
+  }
+}
---
+++ Practics7.Task4/MyList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practics7.Task4
+{
+  internal class MyList : IEnumerable
+  {
+    public List<int> items { get; set; }
+    public IEnumerator GetEnumerator()
+    {
+      return this.items.GetEnumerator();
+    }
+  }
+}
